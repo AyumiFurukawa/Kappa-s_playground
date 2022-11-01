719eef5213b0ad7ba967c798c2c5d10ddefed302
AyumiFurukawa <ayumi.furukawa.0616@gmail.com>
2022-11-01 14:47:17 +0900

    db設計と諸々の微修正

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -30,32 +30,32 @@
                     <i class="fas fa-bars"></i>
                 </button>
                 <div class="tm-site-header">
-                    <div class="mb-3 mx-auto tm-site-logo"><i class="fas fa-regular fa-laptop"></i></div>
-                    <h1 class="text-center">Kappa's Playground</h1>
+                    <div class="mb-3 mx-auto tm-site-logo"><i class="fas fa-times fa-2x"></i></div>
+                    <h1 class="text-center">Xtra Blog</h1>
                 </div>
                 <nav class="tm-nav" id="tm-nav">
                     <ul>
-                        <li class="tm-nav-item"><a href="index.html" class="tm-nav-link">
+                        <li class="tm-nav-item active"><a href="index.html" class="tm-nav-link">
                             <i class="fas fa-home"></i>
                             Blog Home
-                        </a></li>
-                        <li class="tm-nav-item"><a href="contact.html" class="tm-nav-link">
-                            <i class="fas fa-book fa-solid "></i>
-                            Index
                         </a></li>
                         <li class="tm-nav-item"><a href="post.html" class="tm-nav-link">
                             <i class="fas fa-pen"></i>
                             Single Post
                         </a></li>
-                        <li class="tm-nav-item active"><a href="about.html" class="tm-nav-link">
+                        <li class="tm-nav-item"><a href="about.html" class="tm-nav-link">
                             <i class="fas fa-users"></i>
-                            About Me
+                            About Xtra
+                        </a></li>
+                        <li class="tm-nav-item"><a href="contact.html" class="tm-nav-link">
+                            <i class="far fa-comments"></i>
+                            Contact Us
                         </a></li>
                     </ul>
                 </nav>
                 <div class="tm-mb-65">
-                    <a href="https://facebook.com" class="tm-social-link">
-                        <i class="fab fa-github tm-social-icon"></i>
+                    <a rel="nofollow" href="https://fb.com/templatemo" class="tm-social-link">
+                        <i class="fab fa-facebook tm-social-icon"></i>
                     </a>
                     <a href="https://twitter.com" class="tm-social-link">
                         <i class="fab fa-twitter tm-social-icon"></i>
@@ -63,12 +63,29 @@
                     <a href="https://instagram.com" class="tm-social-link">
                         <i class="fab fa-instagram tm-social-icon"></i>
                     </a>
+                    <a href="https://linkedin.com" class="tm-social-link">
+                        <i class="fab fa-linkedin tm-social-icon"></i>
+                    </a>
                 </div>
+                <p class="tm-mb-80 pr-5 text-white">
+                    Xtra Blog is a multi-purpose HTML template from TemplateMo website. Left side is a sticky menu bar. Right side content will scroll up and down.
+                </p>
             </div>
         </header>
 
         @yield('content')
 
+        <footer class="row tm-row">
+            <hr class="col-12">
+            <div class="col-md-6 col-12 tm-color-gray">
+                Design: <a rel="nofollow" target="_parent" href="https://templatemo.com" class="tm-external-link">TemplateMo</a>
+            </div>
+            <div class="col-md-6 col-12 tm-color-gray tm-copyright">
+                Copyright 2020 Xtra Blog Company Co. Ltd.
+            </div>
+        </footer>
 
+        <script src="js/jquery.min.js"></script>
+        <script src="js/templatemo-script.js"></script>
     </body>
 </html>
